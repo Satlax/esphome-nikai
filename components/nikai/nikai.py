db65394d22551364153c2c8e2798feffb40587c7
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 00:40:33 +0300

    Update nikai.py

--- components/nikai/nikai.py
+++ components/nikai/nikai.py
@@ -4,7 +4,11 @@ import esphome.config_validation as cv
 from esphome.components import remote_base
 
 
+DEPENDENCIES = ["remote_base"]
+
+
 nikai_ns = cg.esphome_ns.namespace("remote_base")
+
 
 NikaiData = nikai_ns.struct("NikaiData")
 
@@ -16,18 +20,21 @@ NIKAI_SCHEMA = cv.Schema({
 
 remote_base.declare_protocol("Nikai")
 
-remote_base.register_dumper(
-    "nikai",
-    NikaiData
-)
-
-remote_base.register_trigger(
-    "nikai",
-    NikaiData
-)
 
 remote_base.register_action(
     "nikai",
     NikaiData,
-    NIKAI_SCHEMA
+    NIKAI_SCHEMA,
+)
+
+
+remote_base.register_trigger(
+    "nikai",
+    NikaiData,
+)
+
+
+remote_base.register_dumper(
+    "nikai",
+    NikaiData,
 )
