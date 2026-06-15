8853e8573f7ec214ecf9ea28203b1cb842ab76f2
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-15 19:58:31 +0300

    Update nikai.h

--- components/nikai/nikai.h
+++ components/nikai/nikai.h
@@ -1,1 +1,27 @@
+#pragma once
 
+#include "esphome/core/component.h"
+#include "esphome/components/remote_receiver/remote_receiver.h"
+
+namespace esphome {
+namespace nikai {
+
+
+class NikaiReceiver : public Component {
+
+ public:
+
+  void setup() override;
+
+  void dump_config() override;
+
+
+ private:
+
+  remote_receiver::RemoteReceiverComponent *receiver_;
+
+};
+
+
+}
+}
