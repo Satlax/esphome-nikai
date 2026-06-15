e4760f3f8ad7387dfc0212314e3d704906538f06
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 00:03:05 +0300

    Update __init__.py

--- components/nikai/__init__.py
+++ components/nikai/__init__.py
@@ -1,22 +1,3 @@
-import esphome.codegen as cg
-from esphome.components.remote_base import (
-    declare_protocol,
-    register_trigger,
-    register_dumper,
-)
-
-DEPENDENCIES = ["remote_base"]
 CODEOWNERS = ["@Satlax"]
-
-# Объявляем протокол — это автоматически создаст классы NikaiData, NikaiTrigger, NikaiDumper
-NikaiData, NikaiBinarySensor, NikaiTrigger, NikaiAction, NikaiDumper = declare_protocol("Nikai")
-
-# Регистрируем триггер для YAML (on_nikai)
-@register_trigger("nikai", NikaiTrigger, NikaiData)
-def nikai_trigger(var, config):
-    pass
-
-# Регистрируем дампер для dump: nikai
-@register_dumper("nikai", NikaiDumper)
-def nikai_dumper(var, config):
-    pass
+DEPENDENCIES = ["remote_base"]
+AUTO_LOAD = ["remote_base"]
