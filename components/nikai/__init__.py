f553674340ff4a7a64f007268d8ddae609097b40
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-15 21:55:13 +0300

    Update __init__.py

--- components/nikai/__init__.py
+++ components/nikai/__init__.py
@@ -1,22 +1,3 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.components import remote_base
-
+CODEOWNERS = ["@Satlax"]
 DEPENDENCIES = ["remote_base"]
 AUTO_LOAD = ["remote_base"]
-
-nikai_ns = cg.esphome_ns.namespace("remote_base")
-
-NikaiData    = nikai_ns.struct("NikaiData")
-NikaiTrigger = nikai_ns.class_("NikaiTrigger")
-NikaiAction  = nikai_ns.class_("NikaiAction")
-NikaiDumper  = nikai_ns.class_("NikaiDumper")
-
-NIKAI_SCHEMA = cv.Schema({
-    cv.Required("data"): cv.hex_uint32_t,
-})
-
-remote_base.declare_protocol("Nikai")
-remote_base.register_trigger("nikai", NikaiTrigger, NikaiData)
-remote_base.register_action("nikai", NikaiAction, NIKAI_SCHEMA)
-remote_base.register_dumper("nikai", NikaiDumper)
