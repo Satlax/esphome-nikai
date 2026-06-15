da91cc815471c04c9dd5631ae504080dafbb6c6a
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-15 19:59:10 +0300

    Update nikai.cpp

--- components/nikai/nikai.cpp
+++ components/nikai/nikai.cpp
@@ -1,1 +1,28 @@
+#include "nikai.h"
 
+#include "esphome/core/log.h"
+
+
+namespace esphome {
+namespace nikai {
+
+
+static const char *TAG = "nikai";
+
+
+void NikaiReceiver::setup() {
+
+  ESP_LOGI(TAG, "NIKAI receiver loaded");
+
+}
+
+
+void NikaiReceiver::dump_config() {
+
+  ESP_LOGCONFIG(TAG, "NIKAI receiver");
+
+}
+
+
+}
+}
