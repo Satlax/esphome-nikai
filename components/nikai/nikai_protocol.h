324cb250298c98638dddeb20255e5b6efb2cc552
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 01:51:31 +0300

    Update nikai_protocol.h

--- components/nikai/nikai_protocol.h
+++ components/nikai/nikai_protocol.h
@@ -2,16 +2,19 @@
 
 #include "esphome/components/remote_base/remote_base.h"
 
+
 namespace esphome {
 namespace remote_base {
 
 
 struct NikaiData {
+
   uint32_t data;
 
   bool operator==(const NikaiData &rhs) const {
     return data == rhs.data;
   }
+
 };
 
 
@@ -19,11 +22,20 @@ class NikaiProtocol : public RemoteProtocol<NikaiData> {
 
  public:
 
-  void encode(RemoteTransmitData *dst,const NikaiData &data) override;
+  void encode(
+      RemoteTransmitData *dst,
+      const NikaiData &data
+  ) override;
 
-  optional<NikaiData> decode(RemoteReceiveData data) override;
 
-  void dump(const NikaiData &data) override;
+  optional<NikaiData> decode(
+      RemoteReceiveData data
+  ) override;
+
+
+  void dump(
+      const NikaiData &data
+  ) override;
 
 };
 
