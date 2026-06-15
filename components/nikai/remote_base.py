bf520835edbdf55234cdb810d6a998398c09995a
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 00:06:58 +0300

    Update remote_base.py

--- components/nikai/remote_base.py
+++ components/nikai/remote_base.py
@@ -1,26 +1,46 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components.remote_base import (
-    declare_protocol,
-    register_trigger,
-    register_action,
-    register_dumper,
-)
 
-DEPENDENCIES = ["remote_base"]
+from esphome.components import remote_base
+
+
+DEPENDENCIES = [
+    "remote_base"
+]
+
 
 nikai_ns = cg.esphome_ns.namespace("remote_base")
 
-NikaiData    = nikai_ns.struct("NikaiData")
-NikaiTrigger = nikai_ns.class_("NikaiTrigger")
-NikaiAction  = nikai_ns.class_("NikaiAction")
-NikaiDumper  = nikai_ns.class_("NikaiDumper")
 
-NIKAI_SCHEMA = cv.Schema({
-    cv.Required("data"): cv.hex_uint32_t,
-})
+NikaiData = nikai_ns.struct("NikaiData")
 
-declare_protocol("Nikai")
-register_trigger("nikai", NikaiTrigger, NikaiData)
-register_action("nikai", NikaiAction, NIKAI_SCHEMA)
-register_dumper("nikai", NikaiDumper)
+
+NIKAI_SCHEMA = cv.Schema(
+    {
+        cv.Required("data"): cv.hex_uint32_t,
+    }
+)
+
+
+remote_base.declare_protocol(
+    "Nikai"
+)
+
+
+remote_base.register_trigger(
+    "nikai",
+    NikaiData
+)
+
+
+remote_base.register_action(
+    "nikai",
+    NikaiData,
+    NIKAI_SCHEMA
+)
+
+
+remote_base.register_dumper(
+    "nikai",
+    NikaiData
+)
