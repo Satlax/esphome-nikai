27bd7e01ccda02866388937b327f96acb3e21af8
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-15 21:05:46 +0300

    Update nikai_protocol.h

--- components/nikai/nikai_protocol.h
+++ components/nikai/nikai_protocol.h
@@ -1,23 +1,34 @@
 #pragma once
-
 #include "esphome/components/remote_base/remote_base.h"
 
-
 namespace esphome {
-namespace nikai {
+namespace remote_base {
 
-
-class NikaiProtocol : public remote_base::RemoteReceiver {
-
- public:
-
-  bool decode(
-      remote_base::RemoteReceiveData data
-  ) override;
-
-
+struct NikaiData {
+  uint32_t data;
+  bool operator==(const NikaiData &rhs) const { return data == rhs.data; }
 };
 
+class NikaiProtocol : public RemoteProtocol<NikaiData> {
+ public:
+  void encode(RemoteTransmitData *dst, const NikaiData &data) override;
+  optional<NikaiData> decode(RemoteReceiveData data) override;
+  void dump(const NikaiData &data) override;
+};
 
-}
-}
+DECLARE_REMOTE_PROTOCOL(Nikai)
+
+template<typename... Ts>
+class NikaiAction : public RemoteTransmitterActionBase<Ts...> {
+  TEMPLATABLE_VALUE(uint32_t, data)
+ public:
+  void set_data(uint32_t data) { data_ = data; }
+  void encode(RemoteTransmitData *dst, Ts... x) override {
+    NikaiData d{};
+    d.data = this->data_.value(x...);
+    NikaiProtocol().encode(dst, d);
+  }
+};
+
+}  // namespace remote_base
+}  // namespace esphome
