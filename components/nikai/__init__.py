12db558ea2de7018f2f2dbb7a65465a3d87ec76d
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-15 19:57:48 +0300

    Update __init__.py

--- components/nikai/__init__.py
+++ components/nikai/__init__.py
@@ -1,1 +1,18 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+from esphome.components import remote_receiver
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["remote_receiver"]
+
+nikai_ns = cg.esphome_ns.namespace("nikai")
+
+NikaiReceiver = nikai_ns.class_(
+    "NikaiReceiver",
+    cg.Component
+)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(NikaiReceiver),
+})
