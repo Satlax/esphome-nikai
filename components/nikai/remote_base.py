3807c6f3c2029dd85a2da0daa40af108f86ccf3f
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 01:58:34 +0300

    Update remote_base.py

--- components/nikai/remote_base.py
+++ components/nikai/remote_base.py
@@ -6,26 +6,30 @@ from esphome.components import remote_base
 
 nikai_ns = cg.esphome_ns.namespace("remote_base")
 
-
 NikaiData = nikai_ns.struct("NikaiData")
 
 
-NIKAI_SCHEMA = cv.Schema({
-    cv.Required("data"): cv.hex_uint32_t,
-})
+NIKAI_SCHEMA = cv.Schema(
+    {
+        cv.Required("data"): cv.hex_uint32_t,
+    }
+)
 
 
 remote_base.declare_protocol("Nikai")
+
 
 remote_base.register_dumper(
     "nikai",
     NikaiData
 )
 
+
 remote_base.register_trigger(
     "nikai",
     NikaiData
 )
+
 
 remote_base.register_action(
     "nikai",
