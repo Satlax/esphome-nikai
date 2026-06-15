32e19fa7867bd2598ee2850f908c078d73a2caac
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 02:11:10 +0300

    Update nikai_protocol.h

--- components/remote_base/nikai_protocol.h
+++ components/remote_base/nikai_protocol.h
@@ -1,50 +1,34 @@
 #pragma once
-
-#include "esphome/core/component.h"
-#include "esphome/core/log.h"
 #include "esphome/components/remote_base/remote_base.h"
-
 
 namespace esphome {
 namespace remote_base {
-namespace nikai {
-
 
 struct NikaiData {
-
   uint32_t data;
-
-  bool operator==(const NikaiData &rhs) const {
-    return data == rhs.data;
-  }
-
+  bool operator==(const NikaiData &rhs) const { return data == rhs.data; }
 };
-
 
 class NikaiProtocol : public RemoteProtocol<NikaiData> {
-
  public:
-
-  void encode(
-      RemoteTransmitData *dst,
-      const NikaiData &data
-  ) override;
-
-
-  optional<NikaiData> decode(
-      RemoteReceiveData data
-  ) override;
-
-
-  void dump(
-      const NikaiData &data
-  ) override;
-
+  void encode(RemoteTransmitData *dst, const NikaiData &data) override;
+  optional<NikaiData> decode(RemoteReceiveData data) override;
+  void dump(const NikaiData &data) override;
 };
-
 
 DECLARE_REMOTE_PROTOCOL(Nikai)
 
+template<typename... Ts>
+class NikaiAction : public RemoteTransmitterActionBase<Ts...> {
+  TEMPLATABLE_VALUE(uint32_t, data)
+ public:
+  void set_data(uint32_t data) { data_ = data; }
+  void encode(RemoteTransmitData *dst, Ts... x) override {
+    NikaiData d{};
+    d.data = this->data_.value(x...);
+    NikaiProtocol().encode(dst, d);
+  }
+};
 
-}
-}
+}  // namespace remote_base
+}  // namespace esphome
