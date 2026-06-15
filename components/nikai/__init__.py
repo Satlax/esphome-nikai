40c1999fb3f487ee683ba6af4ec5a8cb840e35cc
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 00:31:24 +0300

    Update __init__.py

--- components/nikai/__init__.py
+++ components/nikai/__init__.py
@@ -1,17 +1,4 @@
-
-import esphome.codegen as cg
-
-from esphome.components import remote_base
-
 CODEOWNERS = ["@Satlax"]
 DEPENDENCIES = ["remote_base"]
 AUTO_LOAD = ["remote_base"]
-
-
-nikai_ns = cg.esphome_ns.namespace("remote_base")
-
-
-NikaiData = nikai_ns.struct("NikaiData")
-
-
-remote_base.declare_protocol("Nikai")
+MULTI_CONF = True
