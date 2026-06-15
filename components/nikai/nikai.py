97d8acf94bb4555ec03d3d977ec6def985e1aa75
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 00:18:14 +0300

    Create nikai.py

---
+++ components/nikai/nikai.py
@@ -0,0 +1,39 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.components import remote_base
+
+
+nikai_ns = cg.esphome_ns.namespace("remote_base")
+
+
+NikaiData = nikai_ns.struct("NikaiData")
+
+
+NIKAI_SCHEMA = cv.Schema(
+    {
+        cv.Required("data"): cv.hex_uint32_t,
+    }
+)
+
+
+remote_base.declare_protocol("Nikai")
+
+
+remote_base.register_trigger(
+    "nikai",
+    NikaiData
+)
+
+
+remote_base.register_action(
+    "nikai",
+    NikaiData,
+    NIKAI_SCHEMA
+)
+
+
+remote_base.register_dumper(
+    "nikai",
+    NikaiData
+)
