08b48003ad1a68aefbcc8a0442f32f8a9ee65581
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 23:17:26 +0300

    Update nikai_protocol.cpp

--- components/remote_base/nikai_protocol.cpp
+++ components/remote_base/nikai_protocol.cpp
@@ -6,15 +6,7 @@ namespace remote_base {
 
 static const char *TAG = "remote.nikai";
 
-// ПРАВИЛЬНЫЕ тайминги на основе лога:
-// 4000, -521, 2004, -525, 2000, -524...
-// Start: 4000 mark + 500 space (НЕ 4000!)
 static const uint32_t NIKAI_FREQ        = 38000;
-static const uint32_t NIKAI_START_MARK  = 4000;
-static const uint32_t NIKAI_START_SPACE = 500;     // ← ИСПРАВЛЕНО!
-static const uint32_t NIKAI_BIT_MARK    = 520;
-static const uint32_t NIKAI_ONE_SPACE   = 2000;
-static const uint32_t NIKAI_ZERO_SPACE  = 1000;
 static const uint8_t  NIKAI_BITS        = 24;
 
 static uint32_t reverse_bits(uint32_t value, uint8_t nbits) {
@@ -28,42 +20,76 @@ static uint32_t reverse_bits(uint32_t value, uint8_t nbits) {
 
 void NikaiProtocol::encode(RemoteTransmitData *dst, const NikaiData &data) {
   dst->set_carrier_frequency(NIKAI_FREQ);
-  dst->item(NIKAI_START_MARK, NIKAI_START_SPACE);
+  
+  // Start sequence
+  dst->item(4000, 500);
   
   uint32_t wire_value = reverse_bits(data.data, NIKAI_BITS);
   
   for (int i = 0; i < NIKAI_BITS; i++) {
     if ((wire_value >> i) & 1)
-      dst->item(NIKAI_BIT_MARK, NIKAI_ONE_SPACE);
+      dst->item(520, 2000);
     else
-      dst->item(NIKAI_BIT_MARK, NIKAI_ZERO_SPACE);
+      dst->item(520, 1000);
   }
-  dst->mark(NIKAI_BIT_MARK);
+  dst->mark(520);
 }
 
 optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
   NikaiData out{};
   
-  // Start sequence: 4000 mark + 500 space
-  if (!data.expect_item(NIKAI_START_MARK, NIKAI_START_SPACE))
-    return {};
-  
-  uint32_t wire_value = 0;
-  for (int i = 0; i < NIKAI_BITS; i++) {
-    if (!data.expect_mark(NIKAI_BIT_MARK))
-      return {};
-    
-    if (data.expect_space(NIKAI_ONE_SPACE)) {
-      wire_value |= (1u << i);
-    } else if (data.expect_space(NIKAI_ZERO_SPACE)) {
-      // bit is 0
-    } else {
-      return {};
-    }
+  // ОТЛАДКА: Выводим первые 10 значений
+  ESP_LOGD(TAG, "NIKAI decode attempt, first 10 values:");
+  for (int i = 0; i < 10 && i < data.size(); i++) {
+    ESP_LOGD(TAG, "  [%d] = %d", i, data[i]);
   }
   
-  out.data = reverse_bits(wire_value, NIKAI_BITS);
-  return out;
+  // Пробуем вариант 1: mark=4000, space=500
+  if (data.expect_item(4000, 500)) {
+    ESP_LOGD(TAG, "Variant 1 matched: mark=4000, space=500");
+    
+    uint32_t wire_value = 0;
+    for (int i = 0; i < NIKAI_BITS; i++) {
+      if (!data.expect_mark(520))
+        return {};
+      
+      if (data.expect_space(2000)) {
+        wire_value |= (1u << i);
+      } else if (data.expect_space(1000)) {
+        // bit is 0
+      } else {
+        return {};
+      }
+    }
+    
+    out.data = reverse_bits(wire_value, NIKAI_BITS);
+    return out;
+  }
+  
+  // Пробуем вариант 2: mark=4000, space=4000
+  if (data.expect_item(4000, 4000)) {
+    ESP_LOGD(TAG, "Variant 2 matched: mark=4000, space=4000");
+    
+    uint32_t wire_value = 0;
+    for (int i = 0; i < NIKAI_BITS; i++) {
+      if (!data.expect_mark(520))
+        return {};
+      
+      if (data.expect_space(2000)) {
+        wire_value |= (1u << i);
+      } else if (data.expect_space(1000)) {
+        // bit is 0
+      } else {
+        return {};
+      }
+    }
+    
+    out.data = reverse_bits(wire_value, NIKAI_BITS);
+    return out;
+  }
+  
+  ESP_LOGD(TAG, "No variant matched");
+  return {};
 }
 
 void NikaiProtocol::dump(const NikaiData &data) {
