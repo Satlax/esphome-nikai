16d699f7c1ff3ebd597aedb30293113a49c6f2e8
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 15:52:00 +0300

    Update nikai_protocol.cpp

--- components/remote_base/nikai_protocol.cpp
+++ components/remote_base/nikai_protocol.cpp
@@ -7,37 +7,45 @@ namespace remote_base {
 static const char *TAG = "remote.nikai";
 
 static const uint32_t NIKAI_FREQ        = 38000;
+
 static const uint32_t NIKAI_START_MARK  = 4000;
 static const uint32_t NIKAI_START_SPACE = 4000;
-static const uint32_t NIKAI_BIT_MARK    = 500;
-static const uint32_t NIKAI_ONE_SPACE   = 2000;
-static const uint32_t NIKAI_ZERO_SPACE  = 1000;
-static const uint8_t NIKAI_BITS          = 24;
+
+static const uint32_t NIKAI_BIT_MARK    = 450;
+static const uint32_t NIKAI_ONE_SPACE   = 2050;
+static const uint32_t NIKAI_ZERO_SPACE  = 1050;
+
+static const uint8_t NIKAI_BITS = 24;
 
 
 void NikaiProtocol::encode(RemoteTransmitData *dst, const NikaiData &data) {
-
   dst->set_carrier_frequency(NIKAI_FREQ);
 
   dst->item(
-    NIKAI_START_MARK,
-    NIKAI_START_SPACE
+      NIKAI_START_MARK,
+      NIKAI_START_SPACE
   );
 
   for (int i = NIKAI_BITS - 1; i >= 0; i--) {
 
-    if ((data.data >> i) & 1)
+    if ((data.data >> i) & 1) {
+
       dst->item(
-        NIKAI_BIT_MARK,
-        NIKAI_ONE_SPACE
+          NIKAI_BIT_MARK,
+          NIKAI_ONE_SPACE
       );
-    else
+
+    } else {
+
       dst->item(
-        NIKAI_BIT_MARK,
-        NIKAI_ZERO_SPACE
+          NIKAI_BIT_MARK,
+          NIKAI_ZERO_SPACE
       );
+
+    }
   }
 
+  dst->mark(NIKAI_BIT_MARK);
 }
 
 
@@ -46,9 +54,11 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
   NikaiData out{};
 
   if (!data.expect_item(
-        NIKAI_START_MARK,
-        NIKAI_START_SPACE))
+          NIKAI_START_MARK,
+          NIKAI_START_SPACE
+      )) {
     return {};
+  }
 
 
   uint32_t value = 0;
@@ -56,25 +66,26 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
 
   for (int i = 0; i < NIKAI_BITS; i++) {
 
-    if (!data.expect_mark(NIKAI_BIT_MARK))
+    if (!data.expect_mark(NIKAI_BIT_MARK)) {
       return {};
+    }
 
 
     if (data.expect_space(NIKAI_ONE_SPACE)) {
 
-      value <<= 1;
-      value |= 1;
+      value = (value << 1) | 1;
+
 
     } else if (data.expect_space(NIKAI_ZERO_SPACE)) {
 
       value <<= 1;
+
 
     } else {
 
       return {};
 
     }
-
   }
 
 
@@ -87,13 +98,13 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
 void NikaiProtocol::dump(const NikaiData &data) {
 
   ESP_LOGI(
-    TAG,
-    "Received Nikai: data=0x%06X",
-    data.data
+      TAG,
+      "Received Nikai: data=0x%06X",
+      data.data
   );
 
 }
 
 
-}
-}
+}  // namespace remote_base
+}  // namespace esphome
