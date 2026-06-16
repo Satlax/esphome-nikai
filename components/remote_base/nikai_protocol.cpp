b3df25b5ae58c0821e993b0191463feb7c845777
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-16 22:42:39 +0300

    Update nikai_protocol.cpp

--- components/remote_base/nikai_protocol.cpp
+++ components/remote_base/nikai_protocol.cpp
@@ -9,9 +9,9 @@ static const char *TAG = "remote.nikai";
 static const uint32_t NIKAI_FREQ        = 38000;
 static const uint32_t NIKAI_START_MARK  = 4000;
 static const uint32_t NIKAI_START_SPACE = 4000;
-static const uint32_t NIKAI_ONE_MARK    = 2000;
-static const uint32_t NIKAI_ZERO_MARK   = 1000;
-static const uint32_t NIKAI_BIT_SPACE   = 500;
+static const uint32_t NIKAI_BIT_MARK    = 520;
+static const uint32_t NIKAI_ONE_SPACE   = 2000;
+static const uint32_t NIKAI_ZERO_SPACE  = 1000;
 static const uint8_t  NIKAI_BITS        = 24;
 
 static uint32_t reverse_bits(uint32_t value, uint8_t nbits) {
@@ -26,32 +26,39 @@ static uint32_t reverse_bits(uint32_t value, uint8_t nbits) {
 void NikaiProtocol::encode(RemoteTransmitData *dst, const NikaiData &data) {
   dst->set_carrier_frequency(NIKAI_FREQ);
   dst->item(NIKAI_START_MARK, NIKAI_START_SPACE);
-  // data.data is stored MSB-first (matches Tasmota "Data" representation),
-  // so transmit by sending bit 23 down to bit 0, but the wire format is
-  // LSB-first, which is equivalent to reversing first.
+  
+  // data.data is stored MSB-first (matches Tasmota "Data" representation)
+  // wire format is LSB-first, so reverse first
   uint32_t wire_value = reverse_bits(data.data, NIKAI_BITS);
+  
   for (int i = 0; i < NIKAI_BITS; i++) {
     if ((wire_value >> i) & 1)
-      dst->item(NIKAI_ONE_MARK, NIKAI_BIT_SPACE);
+      dst->item(NIKAI_BIT_MARK, NIKAI_ONE_SPACE);
     else
-      dst->item(NIKAI_ZERO_MARK, NIKAI_BIT_SPACE);
+      dst->item(NIKAI_BIT_MARK, NIKAI_ZERO_SPACE);
   }
+  dst->mark(NIKAI_BIT_MARK);
 }
 
 optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
   NikaiData out{};
   if (!data.expect_item(NIKAI_START_MARK, NIKAI_START_SPACE))
     return {};
+  
   uint32_t wire_value = 0;
   for (int i = 0; i < NIKAI_BITS; i++) {
-    if (data.expect_item(NIKAI_ONE_MARK, NIKAI_BIT_SPACE)) {
+    if (!data.expect_mark(NIKAI_BIT_MARK))
+      return {};
+    
+    if (data.expect_space(NIKAI_ONE_SPACE)) {
       wire_value |= (1u << i);
-    } else if (data.expect_item(NIKAI_ZERO_MARK, NIKAI_BIT_SPACE)) {
-      // bit is 0
+    } else if (data.expect_space(NIKAI_ZERO_SPACE)) {
+      // bit is 0, do nothing
     } else {
       return {};
     }
   }
+  
   out.data = reverse_bits(wire_value, NIKAI_BITS);
   return out;
 }
