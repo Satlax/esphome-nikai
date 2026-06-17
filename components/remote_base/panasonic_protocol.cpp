07bb74fb605cd56db5a5b5c71d622bbdb4aa7adc
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-17 11:08:08 +0300

    Update panasonic_protocol.cpp

--- components/remote_base/panasonic_protocol.cpp
+++ components/remote_base/panasonic_protocol.cpp
@@ -1,13 +1,32 @@
+#include "panasonic_protocol.h"
 #include "nikai_protocol.h"
 #include "esphome/core/log.h"
 
+namespace esphome::remote_base {
 
+static const char *const TAG = "remote.panasonic";
 namespace esphome {
 namespace remote_base {
 
+static constexpr uint32_t HEADER_HIGH_US = 3502;
+static constexpr uint32_t HEADER_LOW_US = 1750;
+static constexpr uint32_t BIT_HIGH_US = 502;
+static constexpr uint32_t BIT_ZERO_LOW_US = 400;
+static constexpr uint32_t BIT_ONE_LOW_US = 1244;
 
+void PanasonicProtocol::encode(RemoteTransmitData *dst, const PanasonicData &data) {
+  dst->reserve(100);
+  dst->item(HEADER_HIGH_US, HEADER_LOW_US);
+  dst->set_carrier_frequency(35000);
 static const char *TAG = "remote.nikai";
 
+  uint32_t mask;
+  for (mask = 1UL << 15; mask != 0; mask >>= 1) {
+    if (data.address & mask) {
+      dst->item(BIT_HIGH_US, BIT_ONE_LOW_US);
+    } else {
+      dst->item(BIT_HIGH_US, BIT_ZERO_LOW_US);
+    }
 
 static const uint32_t NIKAI_FREQ = 38000;
 
@@ -32,8 +51,14 @@ static uint32_t reverse_bits(uint32_t value, uint8_t bits)
     result <<= 1;
     result |= value & 1;
     value >>= 1;
-  }
+}
 
+  for (mask = 1UL << 31; mask != 0; mask >>= 1) {
+    if (data.command & mask) {
+      dst->item(BIT_HIGH_US, BIT_ONE_LOW_US);
+    } else {
+      dst->item(BIT_HIGH_US, BIT_ZERO_LOW_US);
+    }
   return result;
 }
 
@@ -59,12 +84,19 @@ void NikaiProtocol::encode(RemoteTransmitData *dst,
     else
       dst->item(BIT_MARK,ZERO_SPACE);
 
-  }
+}
+  dst->mark(BIT_HIGH_US);
 
 
   dst->mark(BIT_MARK);
 
 }
+optional<PanasonicData> PanasonicProtocol::decode(RemoteReceiveData src) {
+  PanasonicData out{
+      .address = 0,
+      .command = 0,
+  };
+  if (!src.expect_item(HEADER_HIGH_US, HEADER_LOW_US))
 
 
 
@@ -76,8 +108,15 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data)
 
 
   if(!data.expect_item(START_MARK,START_SPACE))
-    return {};
+return {};
 
+  uint32_t mask;
+  for (mask = 1UL << 15; mask != 0; mask >>= 1) {
+    if (src.expect_item(BIT_HIGH_US, BIT_ONE_LOW_US)) {
+      out.address |= mask;
+    } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
+      out.address &= ~mask;
+    } else {
 
   uint32_t value=0;
 
@@ -86,23 +125,30 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data)
   {
 
     if(!data.expect_mark(BIT_MARK))
-      return {};
+return {};
 
 
     if(data.expect_space(ONE_SPACE))
     {
       value |= (1<<i);
-    }
+}
+  }
     else if(data.expect_space(ZERO_SPACE))
     {
 
+  for (mask = 1UL << 31; mask != 0; mask >>= 1) {
+    if (src.expect_item(BIT_HIGH_US, BIT_ONE_LOW_US)) {
+      out.command |= mask;
+    } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
+      out.command &= ~mask;
+    } else {
     }
     else
     {
-      return {};
-    }
+return {};
+}
 
-  }
+}
 
 
   out.data = reverse_bits(value,BITS);
@@ -113,9 +159,11 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data)
       out.data);
 
 
-  return out;
+return out;
 
 }
+void PanasonicProtocol::dump(const PanasonicData &data) {
+  ESP_LOGI(TAG, "Received Panasonic: address=0x%04X, command=0x%08" PRIX32, data.address, data.command);
 
 
 
@@ -129,6 +177,7 @@ void NikaiProtocol::dump(const NikaiData &data)
 
 }
 
+}  // namespace esphome::remote_base
 
 }
 }
