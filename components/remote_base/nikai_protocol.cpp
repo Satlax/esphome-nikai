0fac807a6fbf3bd1e06104f477c11b4e1cbc7e86
Satlax <137489486+Satlax@users.noreply.github.com>
2026-06-17 11:14:20 +0300

    Update nikai_protocol.cpp

--- components/remote_base/nikai_protocol.cpp
+++ components/remote_base/nikai_protocol.cpp
@@ -1,6 +1,7 @@
 #include "nikai_protocol.h"
 #include "esphome/core/log.h"
 
+
 namespace esphome {
 namespace remote_base {
 
@@ -8,21 +9,26 @@ namespace remote_base {
 static const char *TAG = "remote.nikai";
 
 
+static const uint32_t NIKAI_FREQ = 38000;
+
 static const uint32_t START_MARK  = 4000;
 static const uint32_t START_SPACE = 4000;
 
-static const uint32_t BIT_MARK    = 520;
-static const uint32_t ONE_SPACE   = 2000;
-static const uint32_t ZERO_SPACE  = 1000;
+static const uint32_t BIT_MARK = 550;
+
+static const uint32_t ONE_SPACE  = 2000;
+static const uint32_t ZERO_SPACE = 1000;
 
 static const uint8_t BITS = 24;
 
 
-static uint32_t reverse_bits(uint32_t value, uint8_t bits) {
 
+static uint32_t reverse_bits(uint32_t value, uint8_t bits)
+{
   uint32_t result = 0;
 
-  for (uint8_t i = 0; i < bits; i++) {
+  for(uint8_t i=0;i<bits;i++)
+  {
     result <<= 1;
     result |= value & 1;
     value >>= 1;
@@ -32,63 +38,79 @@ static uint32_t reverse_bits(uint32_t value, uint8_t bits) {
 }
 
 
-void NikaiProtocol::encode(RemoteTransmitData *dst, const NikaiData &data) {
 
-  dst->set_carrier_frequency(38000);
+void NikaiProtocol::encode(RemoteTransmitData *dst,
+                           const NikaiData &data)
+{
+
+  dst->set_carrier_frequency(NIKAI_FREQ);
 
   dst->item(START_MARK, START_SPACE);
 
 
-  uint32_t value = reverse_bits(data.data, BITS);
+  uint32_t value = reverse_bits(data.data,BITS);
 
 
-  for (int i = 0; i < BITS; i++) {
+  for(int i=0;i<BITS;i++)
+  {
 
-    dst->item(
-      BIT_MARK,
-      (value >> i) & 1 ? ONE_SPACE : ZERO_SPACE
-    );
+    if(value & (1<<i))
+      dst->item(BIT_MARK,ONE_SPACE);
+    else
+      dst->item(BIT_MARK,ZERO_SPACE);
 
   }
+
+
+  dst->mark(BIT_MARK);
 
 }
 
 
-optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
+
+
+optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data)
+{
 
   NikaiData out{};
 
 
-  if (!data.expect_item(START_MARK, START_SPACE))
+  if(!data.expect_item(START_MARK,START_SPACE))
     return {};
 
 
-  uint32_t value = 0;
+  uint32_t value=0;
 
 
-  for (int i = 0; i < BITS; i++) {
+  for(int i=0;i<BITS;i++)
+  {
 
-
-    if (!data.expect_mark(BIT_MARK))
+    if(!data.expect_mark(BIT_MARK))
       return {};
 
 
-    if (data.expect_space(ONE_SPACE)) {
-
-      value |= (1 << i);
+    if(data.expect_space(ONE_SPACE))
+    {
+      value |= (1<<i);
+    }
+    else if(data.expect_space(ZERO_SPACE))
+    {
 
     }
-
-    else if (!data.expect_space(ZERO_SPACE)) {
-
+    else
+    {
       return {};
-
     }
 
   }
 
 
-  out.data = reverse_bits(value, BITS);
+  out.data = reverse_bits(value,BITS);
+
+
+  ESP_LOGD(TAG,
+      "Decoded Nikai frame: 0x%06" PRIX32,
+      out.data);
 
 
   return out;
@@ -97,13 +119,13 @@ optional<NikaiData> NikaiProtocol::decode(RemoteReceiveData data) {
 
 
 
-void NikaiProtocol::dump(const NikaiData &data) {
 
-  ESP_LOGI(
-    TAG,
-    "Received Nikai: data=0x%06" PRIX32,
-    data.data
-  );
+void NikaiProtocol::dump(const NikaiData &data)
+{
+
+ ESP_LOGI(TAG,
+ "Received Nikai: data=0x%06" PRIX32,
+ data.data);
 
 }
 
